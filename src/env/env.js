8393221ca68a237d3307a804b53003255ae50cc2
Himanshu <294himangshu@gmail.com>
2026-01-21 12:13:56 +0530

    prod url added

--- src/env/env.js
+++ src/env/env.js
@@ -1,3 +1,3 @@
 // export const baseUrl = "https://aivista.co.in/";
-// export const baseUrl = "https://aivista.co.in/souljuntion/";
-export const baseUrl = "http://122.163.121.176:3004/";
+export const baseUrl = "https://aivista.co.in/souljuntion/";
+// export const baseUrl = "http://122.163.121.176:3004/";
